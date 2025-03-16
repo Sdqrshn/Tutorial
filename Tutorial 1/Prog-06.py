54edff4da6e83770a94aec3b774aed0f1d53fd14
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-16 16:59:37 +0530

    Update Prog-06.py

--- Tutorial 1/Prog-06.py
+++ Tutorial 1/Prog-06.py
@@ -1,9 +1,9 @@
-a = float(input("Enter side a: "))
-b = float(input("Enter side b: "))
-c = float(input("Enter side c: "))
+a = float(input("First side"))
+b = float(input("Second side "))
+c = float(input("Third side"))
 
 sides = sorted([a, b, c])
 if sides[0]**2 + sides[1]**2 == sides[2]**2:
-    print("The triangle is a right-angled triangle")
+    print("Right-angled")
 else:
-    print("The triangle is not a right-angled triangle")
+    print("Not right-angled")
