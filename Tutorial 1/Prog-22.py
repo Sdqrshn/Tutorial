0b633f18cbf603bf44e7ee73baaed2dadc4d7902
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-16 18:01:09 +0530

    Update Prog-22.py

--- Tutorial 1/Prog-22.py
+++ Tutorial 1/Prog-22.py
@@ -2,4 +2,4 @@ year = int(input("Enter a year: "))
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print("Leap year")
 else:
-    print("Not a leap year")
+    print("Not leap year")
