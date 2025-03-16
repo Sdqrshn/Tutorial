d90968512684004746c5d90d7d43556379e8284f
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-16 17:45:32 +0530

    Create Prog-17.py

--- Tutorial 1/Prog-17.py
+++ Tutorial 1/Prog-17.py
@@ -1,4 +1,4 @@
-n = int(input("Enter the range for multiplication tables: "))
+n = int(input("Enter a number: "))
 for i in range(1, n + 1):
     for j in range(1, 11):
         print(f"{i} x {j} = {i*j}")
