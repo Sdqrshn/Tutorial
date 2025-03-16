748480963adc4f8492927a938b59fbea9b17b892
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-16 16:52:16 +0530

    Update Prog-05.py

--- Tutorial 1/Prog-05.py
+++ Tutorial 1/Prog-05.py
@@ -8,11 +8,11 @@ d = b**2 - 4*a*c
 if d > 0:
     root1 = (-b + math.sqrt(d)) / (2*a)
     root2 = (-b - math.sqrt(d)) / (2*a)
-    print(f"Roots are real and different: {root1}, {root2}")
+    print(f"{root1}, {root2}")
 elif d == 0:
     root = -b / (2*a)
-    print(f"Roots are real and the same: {root}")
+    print(f"{root}")
 else:
     real = -b / (2*a)
     imag = math.sqrt(-d) / (2*a)
-    print(f"Roots are complex: {real} ± {imag}i")
+    print(f"{real} ± {imag}i")
