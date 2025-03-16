4128d1685377373a11345dad19a2ab5a4ec39e9f
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-16 18:18:26 +0530

    Create Prog-26.py

--- Tutorial 1/Prog-26.py
+++ Tutorial 1/Prog-26.py
@@ -1,7 +1,5 @@
 import math
-
 x1, y1 = (float(x) for x in input("Enter x1, y1: ").split()) 
 x2, y2 = (float(x) for x in input("Enter x2, y2: ").split())
-
 distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 print(f"Distance: {distance:.2f}")
