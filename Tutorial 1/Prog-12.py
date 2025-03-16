cea2fdfa8a5d942bb93343c8cf77e6d25d605f60
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-16 17:33:15 +0530

    Update Prog-12.py

--- Tutorial 1/Prog-12.py
+++ Tutorial 1/Prog-12.py
@@ -1,5 +1,5 @@
 num = [int(input(f"Enter number {i+1}: ")) for i in range(4)]
-p = [n for n in nums if n > 0]
-n = [n for n in nums if n < 0]
+p = [a for a in nums if a > 0]
+n = [a for a in nums if a < 0]
 print(f"Sum of positive nos: {sum(p)}, Average: {sum(p)/len(p) if p else 0}")
 print(f"Sum of negative nos: {sum(n)}, Average: {sum(n)/len(n) if n else 0}")
