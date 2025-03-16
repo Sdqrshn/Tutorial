3d9f7593d0e7f65276747ebef8dfbc22436a8475
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-16 17:26:40 +0530

    Update Prog-10.py

--- Tutorial 1/Prog-10.py
+++ Tutorial 1/Prog-10.py
@@ -2,5 +2,5 @@ n = int(input("Enter number of elements: "))
 nums = []
 for _ in range(n):
     nums.append(int(input("Enter a number: "))) 
-sum_even = sum(num for num in nums if num % 2 == 0)
-print(f"Sum of even numbers: {sum_even}")
+evensum = sum(num for num in nums if num % 2 == 0)
+print(f"Sum of even numbers: {evensum)
