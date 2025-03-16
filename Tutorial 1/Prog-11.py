24c664e1e411d575090cf6249a641a0c1da80306
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-16 17:28:55 +0530

    Update Prog-11.py

--- Tutorial 1/Prog-11.py
+++ Tutorial 1/Prog-11.py
@@ -1,3 +1,3 @@
 n = int(input("Enter a positive integer: "))
-sum_cubes = sum(i**3 for i in range(2, n + 1, 2))
-print(f"Sum of cubes of even numbers ≤ {n}: {sum_cubes}")
+cubesum = sum(i**3 for i in range(2, n + 1, 2))
+print(f"Sum of cubes of even numbers ≤ {n}: {cubesum)
