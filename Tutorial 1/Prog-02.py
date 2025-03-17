0191873bd62067f75f2e408688c1e6bc4a6b5804
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 10:48:22 +0530

    Update Prog-02.py

--- Tutorial 1/Prog-02.py
+++ Tutorial 1/Prog-02.py
@@ -1,5 +1,5 @@
 import math
-radius = float(input("Enter the radius of the circle: "))
+radius = float(input("Enter radius of the circle: "))
 area = math.pi * radius**2
 circumference = 2 * math.pi * radius
 print(f"Area: {area:.2f}")
