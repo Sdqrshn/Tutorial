3f2c265c7c9c88cb1e0f16b7fea4019c1de238a4
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 23:21:51 +0530

    Create Prog-19.py

--- Tutorial 2/Prog-19.py
+++ Tutorial 2/Prog-19.py
@@ -1,4 +1,4 @@
 names = input("Enter names separated by commas: ").split(",")
-names = [name.strip() for name in names]  # Remove extra spaces
+names = [name.strip() for name in names]
 names.sort()
 print("Sorted names:", ", ".join(names))
