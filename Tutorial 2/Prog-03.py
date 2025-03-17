388043f14a08fa75059bd6eebbf0217857d9ebb7
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 10:40:14 +0530

    Create Prog-03.py

--- Tutorial 2/Prog-03.py
+++ Tutorial 2/Prog-03.py
@@ -1,8 +1,8 @@
 string = input("Enter a string: ")
-is_palindrome = True
+palindrome = True
 n = len(string)
 for i in range(n // 2):
     if string[i] != string[n - i - 1]:
-        is_palindrome = False
+        palindrome = False
         break
-print("Palindrome" if is_palindrome else "Not a palindrome")
+print("Palindrome" if palindrome else "Not palindrome")
