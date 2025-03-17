cf198fdec81025ffd805b239a41fc5fa03e37283
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 23:01:33 +0530

    Create Prog-08.py

--- Tutorial 1/Prog-08.py
+++ Tutorial 1/Prog-08.py
@@ -1,3 +1,3 @@
 num = int(input("Enter a number: "))
-sum_of_digits = sum(int(digit) for digit in str(num))
-print(f"Sum of digits: {sum_of_digits}")
+sumofdigits = sum(int(digit) for digit in str(num))
+print(f"Sum of digits: {sumofdigits}")
