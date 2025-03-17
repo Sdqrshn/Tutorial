a0e9dc606a5cb8b90f8b2f0a7f9b53d8444146d7
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 10:39:15 +0530

    Create Prog-02.py

--- Tutorial 2/Prog-02.py
+++ Tutorial 2/Prog-02.py
@@ -1,3 +1,6 @@
 string = input("Enter a string: ")
-result = "".join(string[i] for i in range(len(string)) if i % 2 == 0)
-print(f"String after removing odd index characters: {result}")
+result = ""
+for i in range(len(string)):  
+    if i % 2 == 0: 
+        result += string[i]
+print(f"After removing odd characters: {result}")
