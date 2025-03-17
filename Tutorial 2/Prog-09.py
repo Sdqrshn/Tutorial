8ebbd9564c6c809402a74405beb2ba67ef56d81d
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 10:46:02 +0530

    Create Prog-09.py

--- Tutorial 2/Prog-09.py
+++ Tutorial 2/Prog-09.py
@@ -1,4 +1,4 @@
 string = input("Enter a string: ")
-mid = len(string) // 2
-result = string[:mid][::-1] + string[mid:][::-1]
-print(f"Reversed halves: {result}")
+x = len(string) // 2
+result = string[:x][::-1] + string[x:][::-1]
+print(f"Result: {result}")
