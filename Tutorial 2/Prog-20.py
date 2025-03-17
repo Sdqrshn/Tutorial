0dbfdf88724d144aa758ca8ed6162a6096cc73b6
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 23:24:18 +0530

    Create Prog-20.py

--- Tutorial 2/Prog-20.py
+++ Tutorial 2/Prog-20.py
@@ -1,3 +1,3 @@
-nums = list(map(int, input("Enter numbers separated by spaces: ").split()))
-even_sum = sum(num for num in nums if num % 2 == 0)
-print(f"Sum of even numbers: {even_sum}")
+nums = [int(num) for num in input("Enter numbers: ").split()]
+evensum = sum(num for num in nums if num % 2 == 0)
+print(f"Sum of even numbers: {evensum}")
