6fd0b2f684ca4adfb90beae4a44c5a633e293d65
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 23:16:53 +0530

    Create Prog-15.py

--- Tutorial 2/Prog-15.py
+++ Tutorial 2/Prog-15.py
@@ -1,7 +1,7 @@
-def even_odd(n):
+def evenodd(n):
     print("Even" if n % 2 == 0 else "Odd")
 
-def pos_neg_zero(n):
+def sign(n):
     if n > 0:
         print("Positive")
     elif n < 0:
@@ -12,12 +12,13 @@ def pos_neg_zero(n):
 def factors(n):
     print([i for i in range(1, n + 1) if n % i == 0])
 
-choice = int(input("1. Even or Odd\n2. Positive/Negative/Zero\n3. Factors\nEnter choice: "))
+choice = int(input("1. Even or Odd ?\n2. Sign ?\n3. Factors ?\nEnter choice: "))
 num = int(input("Enter number: "))
+
 if choice == 1:
-    even_odd(num)
+    evenodd(num)
 elif choice == 2:
-    pos_neg_zero(num)
+    sign(num)
 elif choice == 3:
     factors(num)
 else:
