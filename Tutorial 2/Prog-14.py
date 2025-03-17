0612a88acdab0a68405f79a2e3ff0e0ca06f0e9a
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-03-17 23:09:27 +0530

    Create Prog-14.py

--- Tutorial 2/Prog-14.py
+++ Tutorial 2/Prog-14.py
@@ -1,5 +1,9 @@
 import math
+
+def calculate_nCr(n, r):
+    return math.factorial(n) // (math.factorial(r) * math.factorial(n - r))
+
 n = int(input("Enter n: "))
 r = int(input("Enter r: "))
-result = math.factorial(n) // (math.factorial(r) * math.factorial(n - r))
+result = calculate_nCr(n, r)
 print(f"nCr = {result}")
