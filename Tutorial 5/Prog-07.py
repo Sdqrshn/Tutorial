dda82b95da7bf0b35a7be090886b73312eb7f418
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-04-04 06:18:12 +0530

    Delete Tutorial 5/Prog-07.py

--- Tutorial 5/Prog-07.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("auto.csv")
-df.dropna(inplace=True)
-print(df.loc[df['price'].idxmax(), 'company'])
-print(df[df['company'].str.lower() == 'toyota'])
-print(df['company'].value_counts())
-print(df.loc[df.groupby('company')['price'].idxmax()])
-print(df.groupby('company')['average-mileage'].mean())
-print(df.sort_values(by='price', ascending=False))
