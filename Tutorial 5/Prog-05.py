16954d9d5c2ac7475f41412714a578c6ab4f8dfa
Sdqrshn <163304118+Sdqrshn@users.noreply.github.com>
2025-04-04 05:50:34 +0530

    Delete Tutorial 5/Prog-05.py

--- Tutorial 5/Prog-05.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-data = {'Car': ['BMW', 'Benz', 'Tesla'], 'Make': [2000, 2015, 2025], 'Price': [19000,45000,100]}
-df = pd.DataFrame(data)
-df.to_excel("output.xlsx", index=False)
-print("Data written to output.xlsx successfully!")
